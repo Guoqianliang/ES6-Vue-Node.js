dd41008cc37ab74dcbb4724ae3d4cb01415c33f6
Guoqianliang <guoqianliang@foxmail.com>
2021-05-08 22:55:38 +0800

    5.8前端静态页面搭建完成

---
+++ yygh-site/plugins/myPlugin.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'   // 引入vue
+import ElementUI from 'element-ui' //element-ui的全部组件
+import 'element-ui/lib/theme-chalk/index.css'//element-ui的css
+Vue.use(ElementUI) //在vue中使用elementUI
